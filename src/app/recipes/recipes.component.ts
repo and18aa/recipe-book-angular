2edca6d2772602985989de75e6656c93a44ca8d6
Andrei Glisca <andreiglisca@gmail.com>
2019-10-08 19:25:33 +0300

    first commit

---
+++ src/app/recipes/recipes.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+
+@Component({
+  selector: "app-recipes",
+  templateUrl: "./recipes.component.html",
+  styleUrls: ["./recipes.component.css"]
+})
+export class RecipesComponent implements OnInit {
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {}
+}
